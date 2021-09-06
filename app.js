76143134144fddd3102c5efbfd93bf4671fbda73
jagabandhu <jagabandhusahoo94@gmail.com>
2021-09-07 00:25:49 +0530

    wire the fetch function to input text ,click button and output box

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ var btnTranslet = document.querySelector("#btn-translet");
 
 var txtInput = document.querySelector("#txt-input");
 
-var outputDiv = document.querySelector("#output")
+var outputDiv = document.querySelector("#output");
+
+var serverUrl ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+
+function getTransletUrl(text) {
+    return serverUrl +"?"+"text="+ text
+};
 
 
 function clickHandler() {
-    outputDiv.innerHTML = "jhgdsjbhjchdcj" + txtInput.value;
+   // outputDiv.innerHTML = "jhgdsjbhjchdcj" + txtInput.value; 
+   var inputText = txtInput.value; 
+    //textinput
+    //calling server for processing
+   fetch(getTransletUrl(inputText))
+   .then(response => response.json())
+   .then(json=>console.log(json))
 
-}
+};
 
 btnTranslet.addEventListener("click",clickHandler)
